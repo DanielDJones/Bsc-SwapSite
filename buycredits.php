b328bb0a3be626846dfc14c9d8a03f95a47652e4
DanielDJones <j1492189@student.southstaffs.ac.uk>
2017-10-25 15:22:52 +0100

    Made Listing and currency page

---
+++ buycredits.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+
+<nav class="yellow darken-2">
+  <div class="container">
+    <div class="nav-wrapper">
+      <a href="index.html" class="brand-logo">Electro Swappers</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="listings.php">Listings</a></li>
+        <li><a href="profile.php">My Profile</a></li>
+        <li><a href="ongoingswaps.php">My Swaps</a></li>
+        <li><a href="buycredits.php">Buy Credits</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="row">
+  <div class="col s12">
+    <div class="card yellow darken-2">
+      <div class="card-content white-text">
+        <span class="card-title">Buy Credits</span>
+        <p>Current Credits: Placeholder</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col s12 m3">
+    <div class="card green darken-3 center">
+      <div class="card-content white-text">
+        <span class="card-title"> £1 | 100</span>
+        <p>100</p>
+        <p>-----</p>
+        <a href="#" class="btn-large waves-effect waves-light black-text yellow">Buy</a>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m3">
+    <div class="card green darken-3">
+      <div class="card-content white-text center">
+        <span class="card-title"> £5 | 525</span>
+        <p>500 +5% free</p>
+        <p>-----</p>
+        <a href="#" class="btn-large waves-effect waves-light black-text yellow">Buy</a>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m3">
+    <div class="card green darken-3">
+      <div class="card-content white-text center">
+        <span class="card-title"> £10 | 1100</span>
+        <p>1000 +10% free</p>
+        <p>-----</p>
+        <a href="#" class="btn-large waves-effect waves-light black-text yellow">Buy</a>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m3">
+    <div class="card green darken-3">
+      <div class="card-content white-text center">
+        <span class="card-title"> £20 | 2400</span>
+        <p>2000 +20% free</p>
+        <p>-----</p>
+        <a href="#" class="btn-large waves-effect waves-light black-text yellow">Buy</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<footer class="page-footer green darken-2">
+  <div class="footer-copyright">
+    <div class="container">
+    © 2017 Daniel Jones
+    <a class="grey-text text-lighten-4 right" href="#x">Back to top</a>
+    </div>
+  </div>
+</footer>
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
---
+++ listing.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+
+<nav class="yellow darken-2">
+  <div class="container">
+    <div class="nav-wrapper">
+      <a href="index.html" class="brand-logo">Electro Swappers</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="listings.php">Listings</a></li>
+        <li><a href="profile.php">My Profile</a></li>
+        <li><a href="ongoingswaps.php">My Swaps</a></li>
+        <li><a href="buycredits.php">Buy Credits</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="row">
+  <div class="col s12 m4 l2">
+    <div class="card green darken-3">
+      <div class="card-image">
+        <img src="http://via.placeholder.com/400x400">
+      </div>
+
+      <div class="card-action">
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m8 l10">
+    <div class="row">
+      <div class="col s12">
+        <div class="card yellow darken-2">
+          <div class="card-content white-text">
+            <span class="card-title">Listing Title</span>
+          </div>
+          <div class="card-action yellow darken-4">
+            <a href="#">Catagory</a>
+          </div>
+        </div>
+      </div>
+      <div class="col s12">
+        <div class="card green darken-3">
+          <div class="card-content white-text">
+            <span class="card-title">Details</span>
+            <p> Details </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="row">
+  <div class="col s12 m4 l2">
+    <div class="card yellow darken-2">
+      <div class="card-content white-text">
+        <span class="card-title">Area</span>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m8 l10">
+    <div class="card yellow darken-2">
+      <div class="card-content white-text">
+        <span class="card-title">Looking for</span>
+        <p>Lorem ipsum dolor sit amet, volumus corpora mel ei, choro viderer aliquid usu ea. Pro appareat perpetua an, nobis quando tractatos ad est. Te saepe iudico vix. Nec paulo euismod in, duo at dicunt salutatus, hinc lorem nostrum duo et. Ut lorem nostrud sententiae ius, et animal feugait eum, populo iuvaret per et.</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+<div class="row">
+  <div class="col s12">
+    <div class="card green darken-3">
+      <div class="card-content white-text">
+        <span class="card-title">Description</span>
+        <p>Lorem ipsum dolor sit amet, volumus corpora mel ei, choro viderer aliquid usu ea. Pro appareat perpetua an, nobis quando tractatos ad est. Te saepe iudico vix. Nec paulo euismod in, duo at dicunt salutatus, hinc lorem nostrum duo et. Ut lorem nostrud sententiae ius, et animal feugait eum, populo iuvaret per et.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col s12">
+    <div class="card yellow darken-2">
+      <div class="card-content white-text">
+        <span class="card-title">Make a offer</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col s12">
+    <div class="card green darken-3">
+      <div class="card-content white-text">
+        <span class="card-title">Offer</span>
+        <div class="row">
+          <div class="input-field col s12">
+          <textarea id="Offer" class="materialize-textarea"></textarea>
+          <label for="Offer">Offer</label>
+          </div>
+        </div>
+        <div class="row center">
+          <a href="#" class="btn-large waves-effect waves-light black-text yellow">Submit offer</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="page-footer green darken-2">
+  <div class="footer-copyright">
+    <div class="container">
+    © 2017 Daniel Jones
+    <a class="grey-text text-lighten-4 right" href="#x">Back to top</a>
+    </div>
+  </div>
+</footer>
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
